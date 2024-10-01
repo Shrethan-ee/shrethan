f19eacdf3ae5d54e9daad183b06b819d0d842e7c
shrethan <shrethan186@gmail.com>
2024-10-01 22:49:30 +0530

    first

--- assign2/codes/data.c
+++ assign2/codes/data.c
@@ -1,12 +1,5 @@
 #include <stdio.h>
-#include <math.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
 
-#include "libs/matfun.h"
 
 int main() {
 
